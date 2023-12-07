99c5dc26f1956243979fdadff690bc742aac0773
Aman Karmani <aman@tmm1.net>
2023-12-07 00:43:40 +0000

    measure allocated

--- line_profiler/torch.cpp
+++ line_profiler/torch.cpp
@@ -11,8 +11,8 @@
 PY_LONG_LONG
 hpTimer(void)
 {
-        //return c10::cuda::CUDACachingAllocator::getDeviceStats(0).allocated_bytes[0].current;
-        return c10::cuda::CUDACachingAllocator::getDeviceStats(0).reserved_bytes[0].current;
+        return c10::cuda::CUDACachingAllocator::getDeviceStats(0).allocated_bytes[0].current;
+        //return c10::cuda::CUDACachingAllocator::getDeviceStats(0).reserved_bytes[0].current;
 }
 
 double
